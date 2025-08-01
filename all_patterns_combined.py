13f3f0e2404430dae687ef8e076f6de08399ca61
spaghetti-Rudra <chauhanr1udra@gmail.com>
2025-08-01 20:53:25 +0530

    Update all_patterns_combined.py

--- all_patterns_combined.py
+++ all_patterns_combined.py
@@ -36,3 +36,22 @@ def main_reverse():
 # main_star()
 # main_number()
 # main_reverse()
+
+
+
+# Number-guessing-game-
+#The program choose a number between 1 to 100.
+import random
+
+n = random.randint(1, 100)
+guess = int(input("Enter your guess (1 to 100): "))
+#user guess untill it's correct 
+if guess > n:
+    print("Too High!")
+#after each guesses program will check the possibility of the condition
+elif guess < n:
+    print("Too Low!")
+#the number is guessed
+else:
+    print("You guessed right!", n)
+
